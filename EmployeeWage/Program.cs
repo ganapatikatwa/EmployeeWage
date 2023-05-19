45d179a7d5364c59ddf358ab9c59784168cb5d4c
Ganapati <ganapatasakatwa@gmail.com>
2023-05-19 06:35:13 +0530

    [GK] Employee Wage for Part and Full Time

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -25,21 +25,49 @@ namespace EmployeeWage
 
             //UC2--Calculate Daily Employee Wage
 
-            int Present = 1;
-            int WorkingHrs = 8;
+            //int Present = 1;
+            //int WorkingHrs = 8;
+            //int WagePerHr = 20;
+            //Random random= new Random();
+            //int empcheck=random.Next(0,2);
+            //if (empcheck == Present)
+            //{
+            //    Console.WriteLine("Employee is Working Hrs in Day--"+WorkingHrs);
+            //    Console.WriteLine("Employee Wage per Hr--"+WagePerHr);
+            //    int WagePerDay = WorkingHrs * WagePerHr;
+            //    Console.WriteLine("The Wages of Employee Per Day--"+WagePerDay);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Employee Is Absent");
+            //}
+
+            //UC3-- Clculating Wages for Part Time
+            int WorkingHrs = 0;
+            int PartTime = 1;
+            int FullTime = 2;
             int WagePerHr = 20;
             Random random= new Random();
-            int empcheck=random.Next(0,2);
-            if (empcheck == Present)
+            int empcheck= random.Next(0,3);
+            if (empcheck == PartTime)
             {
-                Console.WriteLine("Employee is Working Hrs in Day--"+WorkingHrs);
-                Console.WriteLine("Employee Wage per Hr--"+WagePerHr);
-                int WagePerDay = WorkingHrs * WagePerHr;
-                Console.WriteLine("The Wages of Employee Per Day--"+WagePerDay);
+                WorkingHrs= 8;
+                Console.WriteLine("Employee is Working Part Time");
+                Console.WriteLine("Employee working Part Time and Working Hrs--"+WorkingHrs);
+                int PartTimeWage = WorkingHrs * WagePerHr;
+                Console.WriteLine("The Wages for Part Time Working Employee--"+PartTimeWage);
+            }
+            else if(empcheck==FullTime)
+            {
+                WorkingHrs= 16;
+                Console.WriteLine("Employee is Working Full Time");
+                Console.WriteLine("Employee working Full Time and Working Hrs--" + WorkingHrs);
+                int FullTimeWage = WorkingHrs * WagePerHr;
+                Console.WriteLine("The Wages for Full Time Working Employee--" + FullTimeWage);
             }
             else
             {
-                Console.WriteLine("Employee Is Absent");
+                Console.WriteLine("Employee is Absent");
             }
 
         }

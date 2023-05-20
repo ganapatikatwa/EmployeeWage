7a3ac56b4fec2bc4ef1b6778771cc789632d1056
Ganapati <ganapatasakatwa@gmail.com>
2023-05-20 18:59:48 +0530

    [GK] Using Switch Case

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -43,31 +43,59 @@ namespace EmployeeWage
             //}
 
             //UC3-- Clculating Wages for Part Time
+            //int WorkingHrs = 0;
+            //int PartTime = 1;
+            //int FullTime = 2;
+            //int WagePerHr = 20;
+            //Random random= new Random();
+            //int empcheck= random.Next(0,3);
+            //if (empcheck == PartTime)
+            //{
+            //    WorkingHrs= 8;
+            //    Console.WriteLine("Employee is Working Part Time");
+            //    Console.WriteLine("Employee working Part Time and Working Hrs--"+WorkingHrs);
+            //    int PartTimeWage = WorkingHrs * WagePerHr;
+            //    Console.WriteLine("The Wages for Part Time Working Employee--"+PartTimeWage);
+            //}
+            //else if(empcheck==FullTime)
+            //{
+            //    WorkingHrs= 16;
+            //    Console.WriteLine("Employee is Working Full Time");
+            //    Console.WriteLine("Employee working Full Time and Working Hrs--" + WorkingHrs);
+            //    int FullTimeWage = WorkingHrs * WagePerHr;
+            //    Console.WriteLine("The Wages for Full Time Working Employee--" + FullTimeWage);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Employee is Absent");
+            //}
+
+            //UC4--Using Switch Case Statement
             int WorkingHrs = 0;
             int PartTime = 1;
             int FullTime = 2;
             int WagePerHr = 20;
-            Random random= new Random();
-            int empcheck= random.Next(0,3);
-            if (empcheck == PartTime)
+            Random random = new Random();
+            int empcheck = random.Next(0, 3);
+            Console.WriteLine(empcheck);
+            switch(empcheck)
             {
-                WorkingHrs= 8;
-                Console.WriteLine("Employee is Working Part Time");
-                Console.WriteLine("Employee working Part Time and Working Hrs--"+WorkingHrs);
-                int PartTimeWage = WorkingHrs * WagePerHr;
-                Console.WriteLine("The Wages for Part Time Working Employee--"+PartTimeWage);
-            }
-            else if(empcheck==FullTime)
-            {
-                WorkingHrs= 16;
-                Console.WriteLine("Employee is Working Full Time");
-                Console.WriteLine("Employee working Full Time and Working Hrs--" + WorkingHrs);
-                int FullTimeWage = WorkingHrs * WagePerHr;
-                Console.WriteLine("The Wages for Full Time Working Employee--" + FullTimeWage);
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
+                case 1: 
+                    WorkingHrs=8;
+                    Console.WriteLine("Employee Working Hrs is--" + WorkingHrs);
+                    int PartTimeWage = WorkingHrs * WagePerHr;
+                    Console.WriteLine("Part Time Wage--"+PartTimeWage);
+                    break;
+                case 2:
+                    WorkingHrs = 16;
+                    Console.WriteLine("Employee Working Hrs is--" + WorkingHrs);
+                    int FullTimeWage = WorkingHrs * WagePerHr;
+                    Console.WriteLine("Part Time Wage--" + FullTimeWage);
+                    break;
+                default:
+                    WorkingHrs = 0;
+                    Console.WriteLine("Employee is Absent");
+                    break;
             }
 
         }
